255f857b1ce385315b6efeca3802111469cab82d
Stijn van der Made <stijnmade@gmail.com>
2023-01-06 20:07:05 +0100

    change db import

--- FlaskApp/db.py
+++ FlaskApp/db.py
@@ -1,13 +1,13 @@
 import sqlite3
 
 import click
-from flask import current_app, g
+from . import current_app, g
 
 
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config['.\\FlaskApp\\FlaskApp.db'],
+            current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
