0079d0ebc0f5720dd699628f1f729a36786de362
Stijn van der Made <stijnmade@gmail.com>
2023-01-06 20:12:21 +0100

    add database route

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+app.config.from_mapping(
+   SECRET_KEY='dev',
+   DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+   )
 
 @app.route('/test')
 def test():
