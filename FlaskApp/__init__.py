4c863735cf64c7f4d52dad2e934c6b39392935cc
Stijn van der Made <stijnmade@gmail.com>
2023-01-06 20:40:36 +0100

    fix db?

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
    SECRET_KEY='dev',
-   DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+   DATABASE=os.path.join(app.instance_path, 'db', 'db.sqlite'),
    )
 
 @app.route('/test')
