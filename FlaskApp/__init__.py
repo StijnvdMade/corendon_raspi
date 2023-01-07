f9559dd2958863f7bb7747be4b7b8dce6a472272
Stijn van der Made <stijnmade@gmail.com>
2023-01-07 13:30:42 +0100

    fix db init?

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,48 +1,51 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, instance_relative_config=True)
-# print(os.path.join('/var/www/corendon_raspi/FlaskApp/db', 'db.sqlite'))
-app.config.from_mapping(
-   SECRET_KEY='dev',
-   DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-   )
+def create_app(test_config=None):
+   app = Flask(__name__, instance_relative_config=True)
+   # print(os.path.join('/var/www/corendon_raspi/FlaskApp/db', 'db.sqlite'))
+   app.config.from_mapping(
+      SECRET_KEY='dev',
+      DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+      )
 
 
-# if test_config is None:
-#    # load the instance config, if it exists, when not testing
-#    app.config.from_pyfile('config.py', silent=True)
-# else:
-#    # load the test config if passed in
-#    app.config.from_mapping(test_config)
+   if test_config is None:
+      # load the instance config, if it exists, when not testing
+      app.config.from_pyfile('config.py', silent=True)
+   else:
+      # load the test config if passed in
+      app.config.from_mapping(test_config)
 
-# ensure the instance folder exists
-try:
-   os.makedirs(app.instance_path)
-except OSError:
-   pass
+   # ensure the instance folder exists
+   try:
+      os.makedirs(app.instance_path)
+   except OSError:
+      pass
 
-@app.route('/test')
-def test():
-   return "I can successfully copy and paste!"
+   @app.route('/test')
+   def test():
+      return "I can successfully copy and paste!"
 
-@app.route('/temp')
-def temp():
-    return "deze pagina werkt"
+   @app.route('/temp')
+   def temp():
+      return "deze pagina werkt"
 
-if __name__ == "__main__":
-   app.run()
+   if __name__ == "__main__":
+      app.run()
 
-from . import db
-db.init_app(app)
+   from . import db
+   db.init_app(app)
 
-from . import auth
-app.register_blueprint(auth.bp)
+   from . import auth
+   app.register_blueprint(auth.bp)
 
-@app.route('/')
-def login():
-   return render_template('login.html')
+   @app.route('/')
+   def login():
+      return render_template('login.html')
 
-@app.route('/en')
-def login_en():
-   return render_template('login_en.html')
+   @app.route('/en')
+   def login_en():
+      return render_template('login_en.html')
+   
+   return app
