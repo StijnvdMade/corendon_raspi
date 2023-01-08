bc463291dc2d0707d8e14168d2bcbda55bcfc1f8
Stijn van der Made <stijnmade@gmail.com>
2023-01-08 16:50:14 +0100

    dummy data

--- FlaskApp/schema.sql
+++ FlaskApp/schema.sql
@@ -14,3 +14,6 @@ CREATE TABLE Wifi_Registration (
   Ticket_Nummer VARCHAR(10),
   PRIMARY KEY(Ticket_Nummer)
 );
+Insert into Wifi_Registration value(F-05,XD03);
+Insert into Wifi_Registration value(E-08,ZV07);
+Insert into Wifi_Registration value(B-17,XD67);
