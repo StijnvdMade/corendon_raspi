72e14bd102d43d105296fb98e92a7172e24c93f3
Stijn van der Made <stijnmade@gmail.com>
2023-01-10 10:28:37 +0100

    download tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./FlaskApp/templates/**/*.html",
+    "./FlaskApp/static/src/**/*.js"
+  ],
   theme: {
     extend: {},
   },
